fe7ee977432563c25d65975bc7f1d31e999f8c00
jonathankolsrud <jonathan.kolsrud@elev.ga.ntig.se>
2023-03-10 17:23:48 +0100

    2023-03-09

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 const burger = document.getElementById('burger')
-const nav = document.getElementById('nav-links')
+const nav = document.getElementById('left_container')
 
 function toggleMenu() {
     burger.classList.toggle('burger_toggle')
